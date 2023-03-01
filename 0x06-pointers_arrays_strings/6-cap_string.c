ae7aaf9f969f4102c743083847214323f1dca21f
Kakwiri <97972926+KakwiriStephen@users.noreply.github.com>
2023-03-01 23:10:19 +0300

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,28 @@
 #include "main.h"
+#include <stdio.h>
 
-#define
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void){
+  
+char *cap_string(char *s)
+
+  int i, j;
+  char sep[] = " \t\n,;.!?\"(){}";
+
+  for (i = 0; s[i] != '\0'; i++)
+  {
+    if (i == 0 || s[i - 1] == sep[0] || s[i - 1] == sep[1] || s[i - 1] == sep[2]
+        || s[i - 1] == sep[3] || s[i - 1] == sep[4] || s[i - 1] == sep[5] || s[i - 1] == sep[6]
+        || s[i - 1] == sep[7] || s[i - 1] == sep[8] || s[i - 1] == sep[9] || s[i - 1] == sep[10]
+        || s[i - 1] == sep[11] || s[i - 1] == sep[12])
+    {
+      if (s[i] >= 'a' && s[i] <= 'z')
+        s[i] -= 32;
+    }
+  }
+  return (s);
+}
